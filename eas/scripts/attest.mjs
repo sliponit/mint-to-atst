29bf8e11f2c3d1d24bba508c8b95600fb5321247
sliponit <sliponit@xps13.home>
2023-10-22 07:36:50 +0200

    feat: iframe thirdweb and attest.mjs

---
+++ eas/scripts/attest.mjs
@@ -0,0 +1,16 @@
+import { EAS, Offchain, SchemaEncoder, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from 'ethers';
+
+export const EASContractAddress = "0x4200000000000000000000000000000000000021"; // OP goerli
+
+// Initialize the sdk with the address of the EAS Schema contract address
+const eas = new EAS(EASContractAddress);
+
+// Gets a default provider (in production use something else like infura/alchemy)
+const provider = ethers.providers.getDefaultProvider(
+  "sepolia"
+);
+
+// Connects an ethers style provider/signingProvider to perform read/write functions.
+// MUST be a signer to do write operations!
+eas.connect(provider);

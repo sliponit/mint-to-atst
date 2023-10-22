077a29fa93e577f2230aa596d3dbedb6c57db075
sliponit <sliponit@xps13.home>
2023-10-22 10:16:48 +0200

    feat: init worker

---
+++ worker/src/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		if (request.method.toUpperCase() === "GET") return new Response("OK");
+    if (request.method.toUpperCase() !== "POST") return new Response("Invalid method");
+
+		try {
+			const body = await request.json()
+			console.log('TODO??', body)
+			return new Response("OK");
+		}  catch (e) {
+      return new Response("Error thrown " + e.message);
+    }
+		
+	},
+};

85910bb86df9e9489d5347a079595df6727f8d61
sliponit <sliponit@xps13.home>
2023-10-22 07:51:58 +0200

    feat: provider with op goerli ok

--- eas/scripts/attest.mjs
+++ eas/scripts/attest.mjs
@@ -7,9 +7,10 @@ export const EASContractAddress = "0x4200000000000000000000000000000000000021"; 
 const eas = new EAS(EASContractAddress);
 
 // Gets a default provider (in production use something else like infura/alchemy)
-const provider = ethers.providers.getDefaultProvider(
-  "sepolia"
-);
+const provider = ethers.getDefaultProvider("optimism-goerli");
+
+const output = await provider.getBlockNumber();
+console.log(output);
 
 // Connects an ethers style provider/signingProvider to perform read/write functions.
 // MUST be a signer to do write operations!
